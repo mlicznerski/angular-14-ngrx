35f58873fb1faaaf5964a310a981aff118286945
mlicznerski <117748840+mlicznerski@users.noreply.github.com>
2022-11-08 17:12:06 +0100

    added bootstrap with basic modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content: TemplateRef<any>) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      });
+  }
+}
